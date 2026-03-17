303230a7fcb34b80eab79e352b8de4246e78423d
Christian99909 <cristian99909@gmail.com>
2026-03-17 07:08:59 -0600

    Deshabilitar tipos anulables y añadir controlador y modelo
    Se ha deshabilitado la característica de tipos anulables en el archivo `ControlPresupuesto.csproj`.
    Se ha añadido el controlador `TiposCuentasController` con un método `Crear` que devuelve una vista.
    Además, se ha creado la clase `TipoCuenta` con propiedades para `Id`, `Nombre`, `UsuarioId` y `Orden`.

---
+++ ControlPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,11 @@
+﻿namespace ControlPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+    }
+}
