bf2fa5886dda8aadd7ab64427db5d415d61ebb26
Christian99909 <cristian99909@gmail.com>
2026-03-18 11:35:41 -0600

    Añadir validaciones y mensajes de error personalizados
    Se ha mejorado la validación en la clase `TipoCuenta` al agregar mensajes de error personalizados para los campos obligatorios y restricciones de longitud.
    
    Además, se ha implementado un resumen de validación en `Crear.cshtml` para mejorar la retroalimentación al usuario sobre errores en el formulario.

--- ControlPresupuesto/Models/TipoCuenta.cs
+++ ControlPresupuesto/Models/TipoCuenta.cs
@@ -5,7 +5,8 @@ namespace ControlPresupuesto.Models
     public class TipoCuenta
     {
         public int Id { get; set; }
-        [Required]
+        [Required (ErrorMessage ="El campo {0} es requerido") ]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage ="El {0} debe tener entre {2} y {1} caracteres")]
         public string Nombre { get; set; }
 
         public int UsuarioId { get; set; }
