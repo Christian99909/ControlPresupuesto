e70a33337d57ea0562d5c1aff5dc6e6f732436df
Christian99909 <cristian99909@gmail.com>
2026-03-19 19:51:46 -0600

    Añadir conexión a la base de datos en TiposCuentasController
    Se han importado los espacios de nombres necesarios para Dapper y SQL Server. Se ha añadido una variable `connectionString` para gestionar la conexión a la base de datos, inicializándola en el constructor. También se ha implementado un bloque `using` para ejecutar una consulta simple a la base de datos.

--- ControlPresupuesto/Controllers/TiposCuentasController.cs
+++ ControlPresupuesto/Controllers/TiposCuentasController.cs
@@ -1,12 +1,27 @@
 ﻿using ControlPresupuesto.Models;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace ControlPresupuesto.Controllers
 {
     public class TiposCuentasController: Controller
     {
+        private readonly string connectionString;
+        public TiposCuentasController(IConfiguration configuration)
+        {
+        
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        }
         public IActionResult Crear() 
         {
+            using (var connection = new SqlConnection(connectionString)) 
+            {
+
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+
+            }
             return View();
 
         }
