62b54a6a90085c854c0c84e32be527ff39666f85
Christian99909 <cristian99909@gmail.com>
2026-04-01 05:18:56 -0600

    Añade RepositorioCuentas para gestión de cuentas
    Se crea la interfaz y clase RepositorioCuentas para manejar la creación de cuentas en la base de datos usando Dapper. Se registra el servicio en Program.cs para permitir la inyección de dependencias. El método Crear inserta una cuenta y asigna su ID generado.

---
+++ ControlPresupuesto/Servicios/RepositorioCuentas.cs
@@ -0,0 +1,31 @@
+﻿using ControlPresupuesto.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace ControlPresupuesto.Servicios
+{
+
+    public interface IRepositorioCuentas
+    {
+        Task Crear(Cuenta cuenta);
+    }
+    public class RepositorioCuentas: IRepositorioCuentas
+    {
+        private readonly string connectionString;
+        public RepositorioCuentas(IConfiguration configuration) 
+        {
+
+            connectionString = configuration.GetConnectionString("DeafultConnection");
+
+        }
+
+        public async Task Crear(Cuenta cuenta) 
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync(@"INSERT INTO Cuentas (Nombre, TipoCuentaId, Descripcion, Balance) VALUES (@Nombre, @TipoCuentaId, @Descripcion, @Balance);    SELECT SCOPE_IDENTITY();", cuenta);
+
+            cuenta.Id = id;
+        }
+
+    }
+}
