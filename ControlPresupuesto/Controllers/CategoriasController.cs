d5e7d4f62c03e2455ce81160781a25afa10fd97b
Christian99909 <cristian99909@gmail.com>
2026-05-08 04:58:50 -0600

    Implementar edición de categorías con validación
    Se añade la funcionalidad para editar categorías:
    - Nuevas acciones Editar (GET y POST) en CategoriasController con validación de modelo.
    - Métodos ObtenerPorId y Actualizar en RepositorioCategorias e interfaz.
    - Nueva vista Editar.cshtml con formulario y validaciones.
    Ahora los usuarios pueden modificar categorías existentes desde la interfaz.

--- ControlPresupuesto/Controllers/CategoriasController.cs
+++ ControlPresupuesto/Controllers/CategoriasController.cs
@@ -34,10 +34,53 @@ namespace ControlPresupuesto.Controllers
         [HttpPost]
         public async Task<IActionResult> Crear(Categoria categoria) 
         {
+            if (!ModelState.IsValid)
+            {
+                return View(categoria);
+            }
 
             var usuarioId = servicioUsuarios.ObtenerUsuarioId();
             categoria.UsuarioId = usuarioId;
             await repositorioCategorias.Crear(categoria);
+            return RedirectToAction("Index");
+
+
+        }
+
+        public async Task<IActionResult> Editar(int id) 
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var categoria = await repositorioCategorias.ObtenerPorId(id, usuarioId);
+
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            return View(categoria);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(Categoria categoriaEditar) 
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(categoriaEditar);
+            }
+
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var categoria = await repositorioCategorias.ObtenerPorId(categoriaEditar.Id, usuarioId);
+
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            categoriaEditar.UsuarioId = usuarioId;
+
+            await repositorioCategorias.Actualizar(categoriaEditar);
             return RedirectToAction("Index");
 
 
