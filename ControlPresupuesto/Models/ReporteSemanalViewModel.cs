b27d317584e6a736edb8bf18b7565674524d0a5a
Christian99909 <cristian99909@gmail.com>
2026-05-15 17:45:05 -0600

    Se realiza el código de la acción semanal del controlador de transacciones.
    Se agregan los atributos Ingresos, Gastos, FechaInicio y FechaFin al modelo ResultadoObtenerPorSemana.
    
    Se agrega un el modelo ReporteSemanalViewModel que representa los datos que se van a enviar a la vista.

---
+++ ControlPresupuesto/Models/ReporteSemanalViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ControlPresupuesto.Models
+{
+    public class ReporteSemanalViewModel
+    {
+        public decimal Ingresos => TransaccionPorSemana.Sum(x => x.Ingresos);
+        public decimal Gastos => TransaccionPorSemana.Sum(x => x.Gastos);
+        public decimal Total => Ingresos - Gastos;
+        public DateTime FechaReferencia { get; set; }
+
+        public IEnumerable<ResultadoObtenerPorSemana> TransaccionPorSemana { get; set; }
+
+    }
+}
